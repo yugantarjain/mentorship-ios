5ff604da678bc030033995fb061f2b919cd4bb99
Yugantar Jain <yugantar8jain@gmail.com>
2020-06-09 18:14:15 +0530

    Keyboard Manager class commented [unused for now].

--- mentorship ios/Managers/KeyboardManager.swift
+++ mentorship ios/Managers/KeyboardManager.swift
@@ -1,27 +1,27 @@
+////
+////  KeyboardManager.swift
+////  mentorship ios
+////
+////  Created by Yugantar Jain on 09/06/20.
+////  Copyright © 2020 Yugantar Jain. All rights reserved.
+////
 //
-//  KeyboardManager.swift
-//  mentorship ios
+//import Foundation
+//import Combine
+//import SwiftUI
 //
-//  Created by Yugantar Jain on 09/06/20.
-//  Copyright © 2020 Yugantar Jain. All rights reserved.
+//final class KeyboardManager: ObservableObject {
+//    @Published var keyboardHeight: CGFloat = 0
 //
-
-import Foundation
-import Combine
-import SwiftUI
-
-final class KeyboardManager: ObservableObject {
-    @Published var keyboardHeight: CGFloat = 0
-
-    func observeKeyboardHeight() {
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
-            let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-            self.keyboardHeight = value.height
-        }
-
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {_ in
-            self.keyboardHeight = 0
-        }
-    }
-
-}
+//    func observeKeyboardHeight() {
+//        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+//            let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+//            self.keyboardHeight = value.height
+//        }
+//
+//        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {_ in
+//            self.keyboardHeight = 0
+//        }
+//    }
+//
+//}
