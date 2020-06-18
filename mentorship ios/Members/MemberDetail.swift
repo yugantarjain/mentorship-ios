ba3309968369e8bec65a4873086a3e2f7c346401
Yugantar Jain <yugantar8jain@gmail.com>
2020-06-18 17:31:45 +0530

    profiledetailcellslist refactored and used for member detail. to be reused for profile page now.

--- mentorship ios/Members/MemberDetail.swift
+++ mentorship ios/Members/MemberDetail.swift
@@ -7,34 +7,20 @@
 import SwiftUI
 
 struct MemberDetail: View {
-    var member: MembersModel.MembersResponseData
+    var memberData: MemberProperties
     @State private var showSendRequestSheet = false
     let hideEmptyFields = true
-
+    
     var body: some View {
-        Form {
-            Group {
-                MemberDetailCell(type: .username, value: member.username, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .slackUsername, value: member.slackUsername, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .isMentor, value: member.availableToMentor ?? false ? "Yes" : "No", hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .needsMentor, value: member.needMentoring ?? false ? "Yes" : "No", hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .interests, value: member.interests, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .bio, value: member.bio, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .location, value: member.location, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .occupation, value: member.occupation, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .organization, value: member.organization, hideEmptyFields: hideEmptyFields)
-                MemberDetailCell(type: .skills, value: member.skills, hideEmptyFields: hideEmptyFields)
-            }
-        }
-        .navigationBarTitle(member.name ?? "Member Detail")
-        .navigationBarItems(trailing:
-            Button(action: { self.showSendRequestSheet.toggle() }) {
-                Text("Send Request")
-                    .font(.headline)
-            }
-        )
+        ProfileDetailCellsList(memberData: memberData, hideEmptyFields: hideEmptyFields)
+            .navigationBarTitle(memberData.name ?? "memberData Detail")
+            .navigationBarItems(trailing:
+                Button(action: { self.showSendRequestSheet.toggle() }) {
+                    Text("Send Request")
+                        .font(.headline)
+            })
             .sheet(isPresented: $showSendRequestSheet) {
-                SendRequest(memberID: self.member.id, memberName: self.member.name ?? "-")
+                SendRequest(memberID: self.memberData.id, memberName: self.memberData.name ?? "-")
         }
     }
 }
@@ -42,7 +28,7 @@ struct MemberDetail: View {
 struct MemberDetail_Previews: PreviewProvider {
     static var previews: some View {
         MemberDetail(
-            member: MembersModel.MembersResponseData.init(
+            memberData: MembersModel.MembersResponseData.init(
                 id: 1,
                 username: "username",
                 name: "yugantar",
