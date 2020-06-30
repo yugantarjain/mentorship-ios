1be1f917b5c1015f956f93f30d901c85c0911406
Yugantar Jain <yugantar8jain@gmail.com>
2020-07-01 03:14:10 +0530

    relation screen done.

--- mentorship ios/Views/Relation/Relation.swift
+++ mentorship ios/Views/Relation/Relation.swift
@@ -9,21 +9,41 @@ import SwiftUI
 struct Relation: View {
     //sample data
     @ObservedObject var sampleData = HomeViewModel()
+    @State var showAlert = false
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("With Isabel Costa").font(.title).fontWeight(.heavy)) {
-                    EmptyView()
+                VStack {
+                    HStack {
+                        Text("Vatsal").font(.title).fontWeight(.heavy)
+                        Spacer()
+                        Text("Ends on: Aug 24, 2020").font(.callout)
+                    }
+                    .foregroundColor(DesignConstants.Colors.subtitleText)
+                    
+                    Divider()
+                        .background(DesignConstants.Colors.defaultIndigoColor)
+                }
+                .listRowBackground(DesignConstants.Colors.formBackgroundColor)
+                
+                TasksToDoSection(tasksToDo: sampleData.homeResponseData.tasksToDo) {
+                    self.showAlert.toggle()
                 }
                 
                 TasksDoneSection(tasksDone: sampleData.homeResponseData.tasksDone)
-                    .environment(\.horizontalSizeClass, .regular)
             }
+            .environment(\.horizontalSizeClass, .regular)
             .navigationBarTitle("Current Relation")
             .navigationBarItems(trailing: Button("Add Task") {
                 //add action code
             })
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Mark as completed?"),
+                    primaryButton: .cancel(),
+                    secondaryButton: .default(Text("Confirm")))
+            }
         }
     }
 }
