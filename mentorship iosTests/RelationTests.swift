bd5b5294765d9f80f17bebee41aa660abaa8a7c7
Yugantar Jain <yugantar8jain@gmail.com>
2020-07-26 18:18:27 +0530

    relation service tests done.

---
+++ mentorship iosTests/RelationTests.swift
@@ -0,0 +1,129 @@
+//
+//  RelationTests.swift
+//  Created on 26/07/20
+//  Created for AnitaB.org Mentorship-iOS 
+//
+
+import XCTest
+@testable import mentorship_ios
+
+class RelationTests: XCTestCase {
+    // custom urlsession for mock network calls
+    var urlSession: URLSession!
+    
+    // MARK: - Setup and Tear Down
+
+    override func setUpWithError() throws {
+        // Set url session for mock networking
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        urlSession = URLSession(configuration: configuration)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    // MARK: - Service
+    
+    func testGetCurrentRelationService() throws {
+        // Service
+        let relationService: RelationService = RelationAPI(urlSession: urlSession)
+        
+        // Set mock json and data
+        let mockJSON = RequestStructure(id: 0, mentor: nil, mentee: nil, endDate: 100, notes: "notes")
+        let mockData = try JSONEncoder().encode(mockJSON)
+        
+        // Return data form mock request handler
+        MockURLProtocol.requestHandler = { _ in
+            return (HTTPURLResponse(), mockData)
+        }
+        
+        // Expectation. Used to test async code
+        let expectation = XCTestExpectation(description: "response")
+        
+        relationService.fetchCurrentRelation { resp in
+            XCTAssertEqual(resp.id, 0)
+            XCTAssertEqual(resp.endDate, mockJSON.endDate)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testFetchTasksService() throws {
+        // Service
+        let relationService: RelationService = RelationAPI(urlSession: urlSession)
+        
+        // Set mock json and data
+        let mockJSON = [TaskStructure(id: 0, description: "test task", isDone: true, createdAt: 0, completedAt: 0)]
+        let mockData = try JSONEncoder().encode(mockJSON)
+        
+        // Return data form mock request handler
+        MockURLProtocol.requestHandler = { _ in
+            return (HTTPURLResponse(), mockData)
+        }
+        
+        // Expectation. Used to test async code
+        let expectation = XCTestExpectation(description: "response")
+        
+        relationService.fetchTasks(id: 0) { resp, success in
+            // test count
+            XCTAssertEqual(resp.count, 1)
+            // test data. Use both methods - using literal, using mock json
+            XCTAssertEqual(resp.first?.id, 0)
+            XCTAssertEqual(resp.first?.description, mockJSON.first?.description)
+            // test success
+            XCTAssertEqual(success, true)
+            
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testAddNewTaskService() throws {
+        // Service
+        let relationService: RelationService = RelationAPI(urlSession: urlSession)
+        
+        // Set mock json and data
+        let mockJSON = RelationModel.ResponseData(message: "test", success: true)
+        let mockData = try JSONEncoder().encode(mockJSON)
+        
+        // Return data form mock request handler
+        MockURLProtocol.requestHandler = { _ in
+            return (HTTPURLResponse(), mockData)
+        }
+        
+        // Expectation. Used to test async code
+        let expectation = XCTestExpectation(description: "response")
+        // Make request and test response
+        relationService.addNewTask(newTask: RelationModel.AddTaskData(description: "test add task"), relationID: 0) { resp in
+            XCTAssertEqual(resp.message, mockJSON.message)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testMarkTaskAsCompleteService() throws {
+        // Service
+        let relationService: RelationService = RelationAPI(urlSession: urlSession)
+        
+        // Set mock json and data
+        let mockJSON = RelationModel.ResponseData(message: "test", success: true)
+        let mockData = try JSONEncoder().encode(mockJSON)
+        
+        // Return data form mock request handler
+        MockURLProtocol.requestHandler = { _ in
+            return (HTTPURLResponse(), mockData)
+        }
+        
+        // Expectation. Used to test async code
+        let expectation = XCTestExpectation(description: "response")
+        
+        relationService.markAsComplete(taskID: 0, relationID: 0) { resp in
+            XCTAssertEqual(resp.message, mockJSON.message)
+            XCTAssertEqual(resp.success, true)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+}
