a90eea3143733b4410fecc1358f542bfcbbab5f3
Yugantar Jain <yugantar8jain@gmail.com>
2020-08-14 05:44:16 +0530

    remove pod files from git

--- Pods/Target Support Files/Pods-mentorship ios-mentorship iosUITests/Pods-mentorship ios-mentorship iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mentorship_ios_mentorship_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_mentorship_ios_mentorship_iosUITests
-@end
